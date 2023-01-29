df5085b0a8dc0e0236b2904c43c0792d1a292dd7
Test <slavasvssvssvs@gmail.com>
2023-01-30 01:37:32 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         int rub = 20; // каждые 20 рублей потраченные за билет
-        int ticket2 = 600; // стоимость билета
-        int bonus = ticket2/rub;
+        int ticket = 600; // стоимость билета
+        int bonus = ticket/rub;
 
         System.out.println("Начисленные мили:" + bonus);
     }
